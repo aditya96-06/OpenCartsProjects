d065b76dc8c27877bcb5bb23f3cbcba46e822eb7
aditya <kulkarniadi06@gmail.com>
2025-09-26 18:01:09 +0530

    TC_RF_009 Existing account verification

---
+++ src/test/java/tutorialsninja/register/TC_RF_009.java
@@ -0,0 +1,38 @@
+package tutorialsninja.register;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC_RF_009 {
+	@Test
+	public void verifyRegistringAccountUsingExistingEmail() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://tutorialsninja.com/demo");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Register")).click();
+		
+		driver.findElement(By.id("input-firstname")).sendKeys("Aditya");
+		driver.findElement(By.id("input-lastname")).sendKeys("Kulkarni");
+		driver.findElement(By.id("input-email")).sendKeys("sfsdfnsfsdfs@starkindustries.com");
+		driver.findElement(By.id("input-telephone")).sendKeys("1234567890");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.id("input-confirm")).sendKeys("12345");
+		driver.findElement(By.cssSelector("input[value='1'][name='newsletter']")).click();
+		driver.findElement(By.name("agree")).click();
+		driver.findElement(By.xpath("//input[@value='Continue']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).isDisplayed());
+		
+		driver.quit();
+	}
+
+}
